bc2bc641148f5aa7c0461f7735b9bd758a54c00f
Siddharth Gargate <siddharth.gargate@springernature.com>
2019-06-20 15:40:46 +0530

    display history and select move from history

--- src/index.js
+++ src/index.js
@@ -46,13 +46,30 @@ function Square(props) {
               history: [{
                   squares: Array(9).fill(null)
               }],
+              stepNumber: 0,
               xIsNext: true
           }
       }
+      jumpTo(step) {
+        this.setState({
+          stepNumber: step,
+          xIsNext: (step % 2) === 0,
+        });
+      }
     render() {
         const history = this.state.history;
-        const current = history[history.length-1]
+        const current = history[this.state.stepNumber]
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move)=>{
+            const desc = move ? 'Go to move #' + move : 'Got to game start';
+            return (
+                <li key={move}>
+                    <button onClick= {()=> this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        });
+
         let status;
         if(winner){
             status = 'Winner: ' + winner;
@@ -67,13 +84,13 @@ function Square(props) {
           </div>
           <div className="game-info">
             <div>{ status }</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
     }
     handleClick = (i)=>{
-        const history = this.state.history;
+        const history = this.state.history.slice(0,this.state.stepNumber + 1);
         const current = history[history.length-1]
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -82,7 +99,8 @@ function Square(props) {
         squares[i] = this.state.xIsNext ? 'X': 'O';
         this.setState({
             history: history.concat([{squares:squares}]),
-            xIsNext: !this.state.xIsNext
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length
         });
       }
   }
